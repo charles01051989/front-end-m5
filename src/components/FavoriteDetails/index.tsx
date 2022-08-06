3d106e3e68c2d7897683075892793ff62a5c5f1d
Charles Marques <charles_sjp@yahoo.com.br>
2022-08-05 22:53:03 -0300

    add component favorite item

--- src/components/FavoriteDetails/index.tsx
+++ src/components/FavoriteDetails/index.tsx
@@ -1,11 +1,31 @@
 import * as S from "./style";
+import FavoriteItemList from "components/FavoriteItemList";
 
 const FavoriteDetails = () => {
   return (
     <S.FavoriteDetails>
       <S.FavoriteDetailsTitle>Detalhes dos favoritos</S.FavoriteDetailsTitle>
       <S.FavoriteDetailsButtonGroup></S.FavoriteDetailsButtonGroup>
-      <S.FavoriteDetailsList>Jogos favoritos</S.FavoriteDetailsList>
+      <S.FavoriteDetailsList>
+        <FavoriteItemList
+          header={
+            <S.FavoriteDetailsListTitle>
+              <h4>Item</h4>
+              <h4>Qtd</h4>
+              <h4>Preço</h4>
+            </S.FavoriteDetailsListTitle>
+          }
+          list = {"Lista de Games"}
+          footer={
+            <S.FavoriteDetailsListFooter>
+              <S.FavoriteDetailsListFooterRow>
+                <span>Subtotal</span>
+                <span>R$ 10.00</span>
+              </S.FavoriteDetailsListFooterRow>
+            </S.FavoriteDetailsListFooter>
+            }
+        />
+      </S.FavoriteDetailsList>
     </S.FavoriteDetails>
   );
 };
---
+++ src/components/FavoriteItemList/index.tsx
@@ -0,0 +1,29 @@
+import * as S from "./style";
+import React, { HTMLAttributes } from "react";
+
+type DivType = HTMLAttributes<HTMLDivElement>;
+
+export type FavoriteItemListProps = {
+  header?: React.ReactNode;
+  footer?: React.ReactNode;
+  list: React.ReactNode;
+} & DivType;
+
+const FavoriteItemList = ({
+  header,
+  list,
+  footer,
+  ...props
+}: FavoriteItemListProps) => {
+  return (
+    <div {...props}>
+      {header && <S.FavoriteItemListTitle> {header} </S.FavoriteItemListTitle>}
+
+      <S.FavoriteItemList role="listbox">{list}</S.FavoriteItemList>
+
+      {footer && <footer> {footer} </footer>}
+    </div>
+  );
+};
+
+export default FavoriteItemList;
