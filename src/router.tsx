6ed802815209fde2b473fed3eece1c1e3e230823
Charles Marques <charles_sjp@yahoo.com.br>
2022-08-06 14:15:29 -0300

    add edit user in to manage user component

--- src/router.tsx
+++ src/router.tsx
@@ -4,14 +4,17 @@ import Home from './pages/Home/index'
 import Login from 'pages/Login';
 import Settings from 'pages/Settings';
 import ManageGames from 'components/ManageGames';
+import ManageUsers from 'components/ManageUsers';
 
 const Router = () => {
     return (
         <Routes>
             <Route path={RoutePath.LOGIN} element={<Login/>} />
             <Route path={RoutePath.HOME} element={<Home />} />
-            <Route path={RoutePath.SETTINGS} element={<Settings/>} />
-            <Route path={RoutePath.SETTINGS_GAMES} element={<ManageGames />} />
+            <Route path={RoutePath.SETTINGS} element={<Settings/>} >
+                <Route path={RoutePath.SETTINGS_GAMES} element={<ManageGames />} />
+                <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
+                </Route>
         </Routes>
     );
 }
